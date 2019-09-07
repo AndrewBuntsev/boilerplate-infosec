a607043de3c20721de555b9273f36d1d5da01152
andreibuntsev <andrew.buntsev@gmail.com>
2019-09-07 20:42:37 +1000

    Added bcrypt password and compare

--- myApp.js
+++ myApp.js
@@ -10,6 +10,36 @@ const helmet = require('helmet');
 
 const bcrypt = require('bcrypt');
 
+
+const saltRounds = 12;
+const myPlaintextPassword = 'sUperpassw0rd!';
+const someOtherPlaintextPassword = 'pass123';
+
+
+//START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  if (err){
+    console.error(err);
+  } else {
+    console.log(hash);
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => { 
+      if (err){
+        console.error(err);
+      } else {
+        console.log(res);
+      }   
+    });
+    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => { 
+      if (err){
+        console.error(err);
+      } else {
+        console.log(res);
+      }   
+    });
+  }
+});
+
+
 // ----
 
 /** - Challenges - *
